49c2a6de0b2799ae216dd85eb45e550851d66fdf
Sebastian Biscussi <sebastian.biscussi@dowjones.com>
2023-09-10 20:07:49 -0300

    add recipe form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import { useState } from 'react'
-import FirebaseAuthService from './FirebaseAuthService'
+import { useState } from 'react';
+import FirebaseAuthService from './FirebaseAuthService';
 
-import './App.css'
-import LoginForm from './components/LoginForm'
+import './App.css';
+import LoginForm from './components/LoginForm';
+import AddEditRecipeForm from './components/AddEditRecipeForm';
+import FirebaseFirestoreService from './FirebaseFirestoreService';
 
-function App () {
-  const [user, setUser] = useState(null)
+function App() {
+  const [user, setUser] = useState(null);
 
-  FirebaseAuthService.subscribeToAuthChanges(setUser)
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
+
+  async function handleAddRecipe(newRecipe) {
+    try {
+      const response = await FirebaseFirestoreService.createDocument('recipes', newRecipe);
+
+      alert(`successfully created a recipe with an ID = ${response.id}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
         <LoginForm existingUser={user}></LoginForm>
       </div>
+      <div className="main">
+        {user ? <AddEditRecipeForm handleAddRecipe={handleAddRecipe}></AddEditRecipeForm> : null}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
