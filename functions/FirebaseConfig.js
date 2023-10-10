62d93050ddd9261156748fb8805ba692da4d6a92
Sebastian Biscussi <sebastian.biscussi@dowjones.com>
2023-10-09 21:07:06 -0300

    project id config + crud api

--- functions/FirebaseConfig.js
+++ functions/FirebaseConfig.js
@@ -2,10 +2,12 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 const FIREBASE_STORAGE_BUCKET = 'fir-recipes-b9125.appspot.com';
+const FIREBASE_PROJECT_ID = 'fir-recipes-b9125';
 
 const apiFirebaseOptions = {
   ...functions.config().firebase,
   credential: admin.credential.applicationDefault(),
+  projectId: FIREBASE_PROJECT_ID,
 };
 
 admin.initializeApp(apiFirebaseOptions);
--- functions/recipesApi.js
+++ functions/recipesApi.js
@@ -50,8 +50,133 @@ app.post('/recipes', async (req, res) => {
     res.status(400).send(error.message);
   }
 });
-app.get('/', (req, res) => {
-  res.send('Hello from firebase function express API');
+app.get('/recipes', async (req, res) => {
+  const authorizationHeader = req.headers['authorization'];
+  const queryObject = req.query;
+  const category = queryObject['category'] ? queryObject['category'] : '';
+  const orderByField = queryObject['orderByField'] ? queryObject['orderByField'] : '';
+  const orderByDirection = queryObject['orderByDirection']
+    ? queryObject['orderByDirection']
+    : 'asc';
+  const pageNumber = queryObject['pageNumber'] ? queryObject['pageNumber'] : '';
+  const perPage = queryObject['perPage'] ? queryObject['perPage'] : '';
+
+  let isAuth = false;
+  let collectionRef = firestore.collection('recipes');
+
+  try {
+    await Utilities.authorizeUser(authorizationHeader, auth);
+    isAuth = true;
+  } catch (error) {
+    collectionRef = collectionRef.where('isPublished', '==', true);
+    console.log(error.message);
+  }
+
+  if (category) collectionRef = collectionRef.where('category', '==', category);
+  if (orderByField) collectionRef.orderBy(orderByField, orderByDirection);
+  if (perPage) collectionRef = collectionRef.limit(Number(perPage));
+  if (pageNumber > 0 && perPage) {
+    const pageNumberMultiplier = pageNumber - 1;
+    const offset = pageNumberMultiplier * perPage;
+    collectionRef = collectionRef.offset(offset);
+  }
+
+  let recipeCount = 0;
+  let countDocRef;
+
+  if (isAuth) {
+    countDocRef = firestore.collection('recipeCounts').doc('all');
+  } else {
+    countDocRef = firestore.collection('recipeCounts').doc('published');
+  }
+
+  const countDoc = await countDocRef.get();
+
+  if (countDoc.exists) {
+    const countDocData = countDoc.data();
+    if (countDocData) recipeCount = countDocData.count;
+  }
+
+  try {
+    const firestoreResponse = await collectionRef.get();
+    console.log('______firestoreResponse.size______', firestoreResponse.size);
+    // const countFetchedRecipes = firestoreResponse
+    const fetchedRecipes = firestoreResponse.docs.map((recipe) => {
+      const id = recipe.id;
+      const data = recipe.data();
+      data.publishDate = data.publishDate._seconds;
+
+      return { ...data, id };
+    });
+
+    const payload = {
+      recipeCount,
+      documents: fetchedRecipes,
+    };
+
+    res.status(200).send(payload);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.put('/recipes/:id', async (req, res) => {
+  const authorizationHeader = req.headers['authorization'];
+
+  if (!authorizationHeader) {
+    res.status(401).send('Missing auth header');
+    return;
+  }
+
+  try {
+    await Utilities.authorizeUser(authorizationHeader, auth);
+  } catch (error) {
+    res.status(401).send(error.message);
+    return;
+  }
+
+  const id = req.params.id;
+  const newRecipe = req.body;
+  const missingFields = Utilities.validateRecipePostPut(newRecipe);
+
+  if (missingFields) {
+    res.status(400).send(`Recipe is not valid. Missing/invalid fields: ${missingFields}`);
+    return;
+  }
+
+  const recipe = Utilities.sanitizeRecipePostPut(newRecipe);
+
+  try {
+    await firestore.collection('recipes').doc(id).set(recipe);
+
+    res.status(200).send({ id });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.delete('recipes/:id', async (req, res) => {
+  const authorizationHeader = req.headers['authorization'];
+
+  if (!authorizationHeader) {
+    res.status(401).send('Missing auth header');
+    return;
+  }
+
+  try {
+    await Utilities.authorizeUser(authorizationHeader, auth);
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+
+  const id = req.params.id;
+
+  try {
+    await firestore.collection('recipes').doc(id).delete();
+    res.status(200).send();
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 if (process.env.NODE_ENV !== 'production') {
